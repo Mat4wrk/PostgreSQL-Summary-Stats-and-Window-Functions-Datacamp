ca1ee5ed208f199828f3b57c910dcd70b9f220a8
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-03-14 23:47:39 +0700

    Create Top, middle, and bottom thirds.sql

---
+++ 2.Fetching, ranking, and paging/Top, middle, and bottom thirds.sql
@@ -0,0 +1,37 @@
+# Split the athletes into top, middle, and bottom thirds based on their count of medals.
+WITH Athlete_Medals AS (
+  SELECT Athlete, COUNT(*) AS Medals
+  FROM Summer_Medals
+  GROUP BY Athlete
+  HAVING COUNT(*) > 1)
+  
+SELECT
+  Athlete,
+  Medals,
+  -- Split athletes into thirds by their earned medals
+  NTILE(3) OVER (ORDER BY Medals DESC) AS Third
+FROM Athlete_Medals
+ORDER BY Medals DESC, Athlete ASC;
+
+
+# Return the average of each third.
+WITH Athlete_Medals AS (
+  SELECT Athlete, COUNT(*) AS Medals
+  FROM Summer_Medals
+  GROUP BY Athlete
+  HAVING COUNT(*) > 1),
+  
+  Thirds AS (
+  SELECT
+    Athlete,
+    Medals,
+    NTILE(3) OVER (ORDER BY Medals DESC) AS Third
+  FROM Athlete_Medals)
+  
+SELECT
+  -- Get the average medals earned in each third
+  Third,
+  AVG(Medals) AS Avg_Medals
+FROM Thirds
+GROUP BY Third
+ORDER BY Third ASC;
