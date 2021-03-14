050249b54794e175e2eab0288568ff872ccefd62
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-03-14 21:44:20 +0700

    Create Last country by name.sql

---
+++ 2.Fetching, ranking, and paging/Last country by name.sql
@@ -0,0 +1,16 @@
+WITH Hosts AS (
+  SELECT DISTINCT Year, City
+    FROM Summer_Medals)
+
+SELECT
+  Year,
+  City,
+  -- Get the last city in which the Olympic games were held
+  LAST_VALUE(City) OVER (
+   ORDER BY Year ASC
+   RANGE BETWEEN
+     UNBOUNDED PRECEDING AND
+     UNBOUNDED FOLLOWING
+  ) AS Last_City
+FROM Hosts
+ORDER BY Year ASC;
