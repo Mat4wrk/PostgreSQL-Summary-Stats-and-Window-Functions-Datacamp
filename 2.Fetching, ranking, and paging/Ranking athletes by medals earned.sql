bc35420a7939a82421032c96272d4c88298c719f
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-03-14 22:31:20 +0700

    Create Ranking athletes by medals earned.sql

---
+++ 2.Fetching, ranking, and paging/Ranking athletes by medals earned.sql
@@ -0,0 +1,14 @@
+WITH Athlete_Medals AS (
+  SELECT
+    Athlete,
+    COUNT(*) AS Medals
+  FROM Summer_Medals
+  GROUP BY Athlete)
+
+SELECT
+  Athlete,
+  Medals,
+  -- Rank athletes by the medals they've won
+  RANK() OVER (ORDER BY Medals DESC) AS Rank_N
+FROM Athlete_Medals
+ORDER BY Medals DESC;
