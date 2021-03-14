b0395e5c9df6b9477d856f1efc4755f1604f3dff
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-03-14 19:52:28 +0700

    Create Numbering Olympic games in descending order.sql

---
+++ 1.Introduction to window functions/Numbering Olympic games in descending order.sql
@@ -0,0 +1,9 @@
+SELECT
+  Year,
+  -- Assign the lowest numbers to the most recent years
+  ROW_NUMBER() OVER (ORDER BY Year DESC) AS Row_N
+FROM (
+  SELECT DISTINCT Year
+  FROM Summer_Medals
+) AS Years
+ORDER BY Year;
