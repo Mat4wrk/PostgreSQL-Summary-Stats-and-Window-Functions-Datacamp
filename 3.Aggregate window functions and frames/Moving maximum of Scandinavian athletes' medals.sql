fa6e641249547830c27892d19c9889f458fa5c13
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-03-16 20:10:03 +0700

    Create Moving maximum of Scandinavian athletes' medals.sql

---
+++ 3.Aggregate window functions and frames/Moving maximum of Scandinavian athletes' medals.sql
@@ -0,0 +1,19 @@
+WITH Scandinavian_Medals AS (
+  SELECT
+    Year, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country IN ('DEN', 'NOR', 'FIN', 'SWE', 'ISL')
+    AND Medal = 'Gold'
+  GROUP BY Year)
+
+SELECT
+  -- Select each year's medals
+  Year,
+  Medals,
+  -- Get the max of the current and next years'  medals
+  MAX(Medals) OVER (ORDER BY Year ASC
+             ROWS BETWEEN
+             CURRENT ROW AND 1 FOLLOWING) AS Max_Medals
+FROM Scandinavian_Medals
+ORDER BY Year ASC;
